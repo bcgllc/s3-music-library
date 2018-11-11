d266f4d14055824c4f6588c1a71388081e33d84b
Justin Harwood <justin@harwoodjp.com>
2018-11-11 17:34:58 -0600

    test->demo.js

---
+++ demo.js
@@ -0,0 +1,26 @@
+const S3MusicLibrary = require("./index")
+require("dotenv").config()
+
+const {
+  AWS_S3_BUCKET,
+  AWS_ACCESS_KEY_ID,
+  AWS_SECRET_ACCESS_KEY,
+} = process.env
+
+const s3MusicLibrary = new S3MusicLibrary(
+  AWS_S3_BUCKET, 
+  AWS_ACCESS_KEY_ID, 
+  AWS_SECRET_ACCESS_KEY
+)
+
+s3MusicLibrary.fetchData()
+  .then(() => {
+    console.log(s3MusicLibrary.artists)
+    // console.log(s3MusicLibrary.albums)
+    // console.log(s3MusicLibrary.tracks)  
+    // console.log(s3MusicLibrary.search("andrew p"))
+    // console.log(s3MusicLibrary.filter({
+    //   artist: "Andrew Pinches",
+    //   album: "Atmos two"
+    // }))
+  })
